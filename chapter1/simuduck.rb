35fe5b7661355882bb300f05e619dc9ca6d5b337
akmatkulov <akmatkulovs991@gmail.com>
2023-09-06 15:03:29 +0600

    Initial app

---
+++ chapter1/simuduck.rb
@@ -0,0 +1,18 @@
+class Duck
+  def quack
+    puts "Quack"
+  end
+  def swim
+    puts "Swimwing to home"
+  end
+
+  def fly
+    puts "I believe a can fly"
+  end
+end
+
+class MallardDuck < Duck
+end
+
+class RedheadDuck < Duck
+end
